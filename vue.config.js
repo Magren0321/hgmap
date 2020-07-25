a3806283633a5627375629f21f046f50089c22ef
Magren0321 <44224328+Magren0321@users.noreply.github.com>
2020-07-25 23:33:18 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+   // 公共路径(必须有的)
+   publicPath: "./",
+   // 输出文件目录
+   outputDir: "dist",
+   // 静态资源存放的文件夹(相对于ouputDir)
+   assetsDir: "static",
+   // eslint-loader 是否在保存的时候检查
+   lintOnSave:false,
+   //打包后小些
+   runtimeCompiler: false,
+   productionSourceMap: true, // 不需要生产环境的设置false可以减小dist文件大小，加速构建
     devServer: {
       port: 8080 // 端口号配置
     },
